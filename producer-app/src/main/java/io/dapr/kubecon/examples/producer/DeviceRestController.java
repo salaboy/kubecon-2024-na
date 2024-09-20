52726cbdb949d4186fd3e400d2d7e8e62d104099
salaboy <Salaboy@gmail.com>
2024-09-20 09:42:21 +0100

    adding subscription

--- producer-app/src/main/java/io/dapr/kubecon/examples/producer/DeviceRestController.java
+++ producer-app/src/main/java/io/dapr/kubecon/examples/producer/DeviceRestController.java
@@ -5,6 +5,8 @@ import io.dapr.spring.messaging.DaprMessagingTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,12 +28,16 @@ public class DeviceRestController {
   @Autowired
   private DaprConnectionDetails daprConnectionDetails;
 
+  @Value("${DAPR_API_TOKEN:}")
+  private String daprApiToken;
+
   private List<DeviceEvent> events = new ArrayList<>();
 
   @PostMapping("/device/events")
   public void iotEvent(@RequestBody DeviceEvent event){
       events.add(event);
       messagingTemplate.send("topic", event);
+      System.out.println("+++ PRODUCING EVENT: " + event);
   }
 
   @GetMapping("/device/events")
@@ -42,8 +48,14 @@ public class DeviceRestController {
 
   @PostMapping("/device/info")
   public InfoRequest requestInfo(@RequestBody InfoRequest request){
-    HttpEntity<InfoRequest> postRequest = new HttpEntity<>(request);
-    return restTemplate.postForObject(daprConnectionDetails.httpEndpoint()+"/v1.0/invoke/"+"consumer-app"+"/method/info", postRequest, InfoRequest.class);
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("dapr-api-token", daprApiToken);
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    HttpEntity<InfoRequest> postRequest = new HttpEntity<>(request, headers);
+
+    String url = daprConnectionDetails.httpEndpoint()+"/v1.0/invoke/consumer-app/method/info";
+    System.out.println("+++ INVOKING SERVICE: " + url + " - Using Token: "+ daprApiToken);
+    return restTemplate.postForObject(url, postRequest, InfoRequest.class);
   }
 
 
