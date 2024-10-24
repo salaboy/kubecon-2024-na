6e5b3a3ddf2bf91b23e505797d5266c457788ac7
Marcos Lilljedahl <marcosnils@gmail.com>
2024-10-24 01:24:34 -0300

    dagger: add kube functions
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

---
+++ .dagger/kube.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"dagger/kubecon-2024-na/internal/dagger"
+)
+
+type Kube struct {
+	// +private
+	K3s *dagger.K3S
+}
+
+// a kubernetes service with all the applications deployed
+func (k *Kube) Service(ctx context.Context) (*dagger.Service, error) {
+	kServer := k.K3s.Server()
+
+	kServer, err := kServer.Start(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = kServer.Endpoint(ctx, dagger.ServiceEndpointOpts{Port: 80, Scheme: "http"})
+	if err != nil {
+		return nil, err
+	}
+
+	dag.Container().From("alpine/helm").
+		WithExec([]string{"apk", "add", "kubectl"}).
+		WithEnvVariable("KUBECONFIG", "/.kube/config").
+		WithFile("/.kube/config", k.K3s.Config()).
+		WithExec([]string{"helm", "install" /*"--wait",*/, "rabbitmq", "oci://registry-1.docker.io/bitnamicharts/rabbitmq"}).
+		WithExec([]string{"helm", "install" /*"--wait",*/, "dapr", "dapr", "--repo", "https://dapr.github.io/helm-charts/", "--version=1.14.1", "--namespace", "dapr", "--create-namespace"}).
+		Sync(ctx)
+
+	// TODO use proxy to export apiserver consumer and producer endpoints
+
+	return kServer, nil
+
+}
+
+func (k *Kube) Config(
+	ctx context.Context,
+	// +default="false"
+	local bool,
+) *dagger.File {
+	return k.K3s.Config(dagger.K3SConfigOpts{Local: local})
+}
+
+func (k *Kube) Kubectl(
+	ctx context.Context,
+	// +optional
+	args string,
+) *dagger.Container {
+	return k.K3s.Kubectl(args)
+}
+
+func (k *Kube) Kns(
+	ctx context.Context,
+) *dagger.Container {
+	return k.K3s.Kns().Terminal()
+}
--- .dagger/main.go
+++ .dagger/main.go
@@ -17,9 +17,6 @@ package main
 import (
 	"context"
 	"dagger/kubecon-2024-na/internal/dagger"
-	"errors"
-	"fmt"
-	"strings"
 )
 
 type Kubecon2024Na struct{}
@@ -69,85 +66,8 @@ func (m *Kubecon2024Na) Consumer(
 	}, nil
 }
 
-func getAppNameAndVersion(ctx context.Context, src *dagger.File) (string, string, error) {
-	out, err := dag.Container().From("mikefarah/yq").WithMountedFile("pom.xml", src).
-		WithExec([]string{"yq", `"\(.project.artifactId):\(.project.version)"`, "pom.xml"}).Stdout(ctx)
-
-	if err != nil {
-		return "", "", err
+func (m *Kubecon2024Na) Kube() *Kube {
+	return &Kube{
+		K3s: dag.K3S("kubecon2024"),
 	}
-
-	nameVersion := strings.Split(out, ":")
-	if len(nameVersion) != 2 {
-		return "", "", errors.New("invalid name/version output")
-	}
-
-	return nameVersion[0], nameVersion[1], nil
-}
-
-func Build(
-	ctx context.Context,
-	appName, appVersion string,
-	src *dagger.Directory,
-) (*dagger.File, error) {
-	name, version, err := getAppNameAndVersion(ctx, src.File("pom.xml"))
-	if err != nil {
-		return nil, err
-	}
-	return dag.Java().WithJdk("17").WithProject(src).
-		Ctr().WithMountedCache("/root/.m2", dag.CacheVolume("kubecon-mvn-cache")).
-		WithExec([]string{"./mvnw", "package", "-DskipTests"}).File(fmt.Sprintf("target/%s-%s.jar", name, version)), nil
-}
-
-func rabbitMQ() *dagger.Container {
-	return dag.Container().From("rabbitmq:3.7.25-management-alpine").
-		WithExposedPort(5672)
-}
-func dapr(ctx context.Context, app string, components *dagger.Directory) func(*dagger.Container) *dagger.Container {
-	return func(c *dagger.Container) *dagger.Container {
-		dapr := dag.Dapr(dagger.DaprOpts{Image: "daprio/daprd:1.14.1"}).
-			Dapr(app, dagger.DaprDaprOpts{
-				AppPort:           8080,
-				AppChannelAddress: app,
-				ComponentsPath:    components,
-			}).WithServiceBinding("rabbitmq", rabbitMQ().AsService()).
-			WithExposedPort(50001).AsService().WithHostname("dapr")
-		dapr.Start(ctx)
-		return c.WithEnvVariable("DAPR_GRPC_ENDPOINT", "http://dapr:50001")
-	}
-}
-
-func Test(
-	ctx context.Context,
-	src *dagger.Directory,
-	daprComponents *dagger.Directory,
-) (*dagger.Service, error) {
-
-	app, _, err := getAppNameAndVersion(ctx, src.File("pom.xml"))
-	if err != nil {
-		return nil, err
-	}
-
-	return dag.Java().WithJdk("17").
-		WithProject(src).
-		Ctr().
-		WithMountedCache("/root/.m2", dag.CacheVolume("kubecon-mvn-cache")).
-		With(dapr(ctx, app, daprComponents)).
-		WithExec([]string{"./mvnw", "test"}).
-		WithExposedPort(8080).
-		AsService().WithHostname(app), nil
-}
-
-func base(
-	ctx context.Context,
-	src *dagger.File,
-	appName string,
-	daprComponents *dagger.Directory,
-) (*dagger.Container, error) {
-	return dag.Container().From("ubuntu/jre:17-22.04_edge").
-		With(dapr(ctx, appName, daprComponents)).
-		WithWorkdir("/usr/src/app").WithFile("app.jar", src).
-		WithMountedTemp("/tmp").
-		WithEntrypoint([]string{"java", "-Dserver.port=8080", "-jar", "app.jar"}).
-		WithExposedPort(8080), nil
 }
